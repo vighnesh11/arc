4384dce5ca6969ef5e7c441e75227a1b5076ab4f
GingerBreadMan11 <47934139+vighnesh11@users.noreply.github.com>
2025-08-31 16:23:25 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,14 +1,10 @@
-# Add the service principal application ID and secret here
-ServicePrincipalId="1c1f52eb-af29-4fff-a3f0-8259f0d347d4";
-ServicePrincipalClientSecret="cKY8Q~DFEqJBgN2C-~BKKBZZwPw_izdJbe_v~b5n";
-
 
 export subscriptionId="8b0fbf42-b133-4d15-a317-1c7456bfd750";
 export resourceGroup="my-rg-01";
 export tenantId="11b76eef-195d-4fda-8672-8688d247437d";
 export location="southeastasia";
-export authType="principal";
-export correlationId="aaeead3b-be7d-4b6b-ae6c-b11f903d7227";
+export authType="token";
+export correlationId="63b977bc-4181-42ea-8e7e-9a12ce379456";
 export cloud="AzureCloud";
 LINUX_INSTALL_SCRIPT="/tmp/install_linux_azcmagent.sh"
 if [ -f "$LINUX_INSTALL_SCRIPT" ]; then rm -f "$LINUX_INSTALL_SCRIPT"; fi;
@@ -17,5 +13,4 @@ if [ $? != 0 ]; then wget -qO- --method=PUT --body-data="{\"subscriptionId\":\"$
 echo "$output";
 bash "$LINUX_INSTALL_SCRIPT";
 sleep 5;
-sudo azcmagent connect --service-principal-id "$ServicePrincipalId" --service-principal-secret "$ServicePrincipalClientSecret" --resource-group "$resourceGroup" --tenant-id "$tenantId" --location "$location" --subscription-id "$subscriptionId" --cloud "$cloud" --tags 'ArcSQLServerExtensionDeployment=Disabled' --correlation-id "$correlationId";
-
+sudo azcmagent connect --resource-group "$resourceGroup" --tenant-id "$tenantId" --location "$location" --subscription-id "$subscriptionId" --cloud "$cloud" --tags 'ArcSQLServerExtensionDeployment=Disabled' --correlation-id "$correlationId";
